7e9de84265096b3317c004122118c21e24e2802b
MariaBlyzniuk <mpechenko210993@gmail.com>
2022-06-10 15:34:59 +0300

    HW_8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+        </a>`
+    }
+    ).join('');
+
+};
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('.feedback-form input')
+const textareaRef = document.querySelector('.feedback-form textarea');
+// const buttonRef = document.querySelector('.feedback-form button');
+
+formRef.addEventListener('submit', onFormSabmit);
+formRef.addEventListener('input', throttle(onFormData, 500));
+
+
+
+const formData = {};
+
+function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+
+
+function onFormSabmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+};
+
+function dataLocalStorage() {
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (data) {
+    inputRef.value = data.email;
+    textareaRef.value = data.message;
+    };
+};a
