c5d112b0523040e7198318bda80f6807c57dcbde
MariaBlyzniuk <mpechenko210993@gmail.com>
2022-06-13 22:23:39 +0300

    update-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,23 +7,14 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(e) {
-    if (playerSet !== null) {
-        localStorage.setItem('videoplayer-current-time', e.seconds);
+    localStorage.setItem('videoplayer-current-time', e.seconds)
+    
+}
+if (localStorage.getItem('videoplayer-current-time')) {
+        player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
     }
-};
-const playerSet = localStorage.getItem('videoplayer-current-time');
 
-player.setCurrentTime(playerSet)
-    .then(function (seconds) {
-    console.log(seconds)
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+
+
+
